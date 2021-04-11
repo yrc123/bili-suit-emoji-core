2a40e68847e54d8c66f41049ae7bf59d96dbe9bc
yrc <958480638@qq.com>
2021-04-11 19:28:21 +0800

    添加 下载背景功能

--- src/main/java/com/yrc/pageprocessor/BiliSuitDetailPageProcessor.java
+++ src/main/java/com/yrc/pageprocessor/BiliSuitDetailPageProcessor.java
@@ -1,23 +1,21 @@
 package com.yrc.pageprocessor;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.yrc.pojo.EmojyItem;
 import com.yrc.pojo.Item;
 import com.yrc.tools.ImageDownloadTool;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
-import us.codecraft.webmagic.selector.Json;
 import us.codecraft.webmagic.selector.JsonPathSelector;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class BiliSuitDetailPageProcessor implements PageProcessor {
 	static final String dataJsonPath = "$.data.suit_items.emoji_package[0].items";
+	static final String bgJsonPath = "$.data.suit_items.space_bg[0].properties";
 	static final String itemIdJsonPath = "$.item_id";
 	static final String nameIdJsonPath = "$.name";
 	static final String imageIdJsonPath = "$.properties.image";
@@ -36,7 +34,22 @@ public class BiliSuitDetailPageProcessor implements PageProcessor {
 			EmojyItem emojyItem = new EmojyItem(itemId, name, image);
 			emojyItems.add(emojyItem);
 		}
-//		page.putField("emojy_items",emojyItems);
+		Map bg = (Map) JSON.parse(page.getRawText());
+		bg= (Map) bg.get("data");
+		bg= (Map) bg.get("suit_items");
+		List list= (List) bg.get("space_bg");
+		bg= (Map) list.get(0);
+		bg= (Map) bg.get("properties");
+		int cnt=0;
+		for (Object o : bg.entrySet()) {
+			Map.Entry entry = (Map.Entry) o;
+			String key = (String) entry.getKey();
+			String value= (String) entry.getValue();
+			if(key.matches(".*portrait.*")){
+				emojyItems.add(new EmojyItem(null,"bg"+cnt,value));
+				cnt++;
+			}
+		}
 		final ImageDownloadTool imageDownloadTool = new ImageDownloadTool(emojyItems, dirPath);
 		imageDownloadTool.startDownload();
 	}
