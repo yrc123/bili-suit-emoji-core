0ce0c0351cfa3fb02d35cc2d350df2ca7f7e1659
yrc <958480638@qq.com>
2021-10-26 22:12:29 +0800

    去除webmagic依赖

---
+++ src/main/java/com/yrc/converter/BiliSuitListConverter.kt
@@ -0,0 +1,79 @@
+package com.yrc.converter
+
+import com.yrc.service.BiliSuitService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class BiliSuitListConverter(val service:BiliSuitService) {
+
+    companion object{
+        //获取套装列表数据的正则表达式
+        const val jsonRegex = "((?<=window.__INITIAL_STATE__=)\\{.*\\})"
+
+        //获取套装id与对应name的正则表达式
+        const val itemRegex = "(\"item_id\":[0-9]*,\"name\":\".*?\")"
+
+        //获取套装id的正则表达式
+        const val itemIdRegex = "((?<=\"item_id\":)[0-9]*)"
+
+        //获取套装name的正则表达式
+        const val nameRegex = "((?<=\"name\":)\".*?\")"
+    }
+    suspend fun getItemMap():Map<Int, String>{
+        val html = service.getSuitList().string()
+        val idNameMap = getIdNameMapByString(html)
+        println("共找到${idNameMap.size}套套装")
+        return idNameMap
+    }
+
+    /**
+     * 通过返回的页面字符串来获取id，name的map
+     * @return
+     */
+    private fun getIdNameMapByString(html: String): Map<Int, String> {
+        //获取json数据
+        val json = Regex(jsonRegex).find(html)?.value?:""
+        val itemIdRegex = Regex(itemIdRegex)
+        val nameRegex = Regex(nameRegex)
+        return Regex(itemRegex)
+            .findAll(json)
+            .map {
+                it.value
+            }
+            .map {
+                (itemIdRegex.find(it)?.value?.toInt()) to (nameRegex.find(it)?.value)
+            }.filter {
+                it.first!=null && it.second!=null
+            }.associate {
+                (it.first!!) to (it.second!!)
+            }
+    }
+    fun printItemList(itemList:List<Pair<Int,String>>){
+        itemList.map {
+                (it.first.toString().padStart(5,' ')) to it.second
+            }
+            .forEach{
+            println("套装id: ${it.first}\t  套装名: ${it.second}")
+        }
+    }
+}
+fun BiliSuitListConverter.printItemList() = runBlocking {
+    val deferred = async {
+        getItemMap().toList()
+    }
+    val itemList = deferred.await()
+    printItemList(itemList)
+}
+fun BiliSuitListConverter.printRegexItemList(regex:String) = runBlocking {
+    val deferred = async {
+        getItemMap().toList()
+    }
+    val itemList = deferred.await()
+    val findRegex = Regex(regex)
+    printItemList(
+        itemList.filter {
+            findRegex.containsMatchIn(it.second)
+        }.toList())
+}
--- src/main/java/com/yrc/main/Main.kt
+++ src/main/java/com/yrc/main/Main.kt
@@ -1,10 +1,24 @@
 package com.yrc.main
 
+import com.yrc.converter.BiliSuitListConverter
+import com.yrc.converter.printItemList
+import com.yrc.converter.printRegexItemList
+import com.yrc.service.BiliSuitService
 import kotlinx.coroutines.runBlocking
+import retrofit2.Retrofit
+import retrofit2.create
 
 class Main {
 
 }
 fun main(args: Array<String>) {
+    val service = Retrofit.Builder()
+        .baseUrl("https://api.bilibili.com/")
+        .build()
+        .create(BiliSuitService::class.java)
 
+    val converter = BiliSuitListConverter(service)
+    converter.printItemList()
+//    converter.printRegexItemList("七海")
 }
+
