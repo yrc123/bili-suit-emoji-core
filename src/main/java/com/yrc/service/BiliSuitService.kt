1dbdf00cbf3bbbef7d10f1f91179775d890a8a5a
yrc <958480638@qq.com>
2021-10-26 17:18:30 +0800

    将项目移植至kotlin，并且去除webmagic依赖

---
+++ src/main/java/com/yrc/service/BiliSuitService.kt
@@ -0,0 +1,14 @@
+package com.yrc.service
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BiliSuitService {
+    //套装详情页api
+    @GET("https://api.bilibili.com/x/garb/mall/item/suit/v2")
+    suspend fun getSuitDetailByItemId(@Query("item_id") itemId:String):ResponseBody
+    //获取套装列表api
+    @GET("https://www.bilibili.com/h5/mall/home")
+    suspend fun getSuitList():ResponseBody
+}
